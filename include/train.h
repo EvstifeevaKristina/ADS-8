8a8036b95c6aac179b01103ddfe9b1115d4c2832
EvstifeevaKristina <94647555+EvstifeevaKristina@users.noreply.github.com>
2022-05-17 18:03:32 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -14,7 +14,6 @@ class Train {
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
   Cage *first; // точка входа в поезд (первый вагон)
  public:
-  Train();
   Train() :first(nullptr), tail(nullptr), countOp(0) {}
   void addCage(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
